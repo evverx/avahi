d76d0a8dd8a9c931000b8aa86bb7046a90d86b92
Evgeny Vereshchagin <evvers@ya.ru>
2022-12-02 20:25:52 +0000

    CI: get leak sanitizer to work

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -31,7 +31,7 @@ case "$1" in
         ;;
     build)
         if [[ "$ASAN_UBSAN" == true ]]; then
-            export CFLAGS="-fsanitize=address,undefined -g"
+            export CFLAGS="-fsanitize=address,undefined -g -fno-omit-frame-pointer"
             export ASAN_OPTIONS=strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1
             export UBSAN_OPTIONS=print_stacktrace=1:print_summary=1:halt_on_error=1
         fi
@@ -42,6 +42,7 @@ case "$1" in
 
         if [[ "$ASAN_UBSAN" == true ]]; then
             sed -i "/\[Service\]/aEnvironment=ASAN_OPTIONS=$ASAN_OPTIONS UBSAN_OPTIONS=$UBSAN_OPTIONS" avahi-daemon/avahi-daemon.service
+            sed -i '/^ExecStart=/s/$/ --no-chroot --no-drop-root/' avahi-daemon/avahi-daemon.service
         fi
 
         make install
