cf92178b5268aa5d312ee654f4abba72a7ccb6f0
Evgeny Vereshchagin <evvers@ya.ru>
2022-12-02 05:20:01 +0000

    CI: bring ASAN/UBSan

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -3,6 +3,8 @@
 set -eux
 set -o pipefail
 
+export ASAN_UBSAN=${ASAN_UBSAN:-false}
+
 case "$1" in
     install-build-deps)
         sed -i -e '/^#\s*deb-src.*\smain\s\+restricted/s/^#//' /etc/apt/sources.list
@@ -22,20 +24,32 @@ case "$1" in
         popd
         ;;
     build)
+        if [[ "$ASAN_UBSAN" == true ]]; then
+            export CFLAGS="-fsanitize=address,undefined -g"
+            export ASAN_OPTIONS=strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1
+            export UBSAN_OPTIONS=print_stacktrace=1:print_summary=1:halt_on_error=1
+	fi
+
         ./bootstrap.sh --enable-tests --prefix=/usr
         make -j"$(nproc)" V=1
         make check VERBOSE=1
+
+        if [[ "$ASAN_UBSAN" == true ]]; then
+            sed -i "/\[Service\]/aEnvironment=ASAN_OPTIONS=$ASAN_OPTIONS UBSAN_OPTIONS=$UBSAN_OPTIONS" avahi-daemon/avahi-daemon.service
+        fi
+
         make install
 
         systemctl daemon-reload
+        systemctl cat avahi-daemon
         if ! systemctl restart avahi-daemon; then
             journalctl -u avahi-daemon -e
             exit 1
-	fi
+        fi
         if ! dfuzzer -v -n org.freedesktop.Avahi; then
             journalctl -u avahi-daemon -b
             exit 1
-	fi
+        fi
         ;;
     *)
         printf '%s' "Unknown command '$1'" >&2
