5a1fd09c67ce2e877f8874087232f42b02224cb7
Evgeny Vereshchagin <evvers@ya.ru>
2022-12-02 04:52:13 +0000

    CI: fuzz DBus interfaces with dfuzzer
    to prevent issues like https://github.com/lathiat/avahi/issues/375

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -10,11 +10,32 @@ case "$1" in
         apt-get build-dep -y avahi
         apt-get install -y libevent-dev qtbase5-dev
         apt-get install -y gcc clang
+        apt-get install -y avahi-daemon
+
+        # install dfuzzer to catch issues like https://github.com/lathiat/avahi/issues/375
+        apt-get install -y libglib2.0-dev meson
+        git clone --depth=1 https://github.com/dbus-fuzzer/dfuzzer
+        pushd dfuzzer
+        meson --buildtype=release build
+        ninja -C ./build -v
+        ninja -C ./build install
+        popd
         ;;
     build)
-        ./bootstrap.sh --enable-tests
+        ./bootstrap.sh --enable-tests --prefix=/usr
         make -j"$(nproc)" V=1
         make check VERBOSE=1
+        make install
+
+        systemctl daemon-reload
+        if ! systemctl restart avahi-daemon; then
+            journalctl -u avahi-daemon -e
+            exit 1
+	fi
+        if ! dfuzzer -v -n org.freedesktop.Avahi; then
+            journalctl -u avahi-daemon -b
+            exit 1
+	fi
         ;;
     *)
         printf '%s' "Unknown command '$1'" >&2
